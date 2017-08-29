967beb17604d6bc62e2454cf44f2d4bd1cecda6a
Domenic Garreffa <garreffd@uwindsor.ca>
2017-08-29 17:00:22 -0400

    Added motivation generation from reddit.com/r/GetMotivated

---
+++ test/timerTest.js
@@ -0,0 +1,16 @@
+/*
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+global.$ = global.jQuery = require('jquery');
+const assert = require("chai").asset;
+const timer = require("../js/timer");
+
+describe("Timer", function(){
+	it("When setLength(25) is executed getLength() should return 25.", function(){
+		timer.setLength(25);
+		assert.equal(timer.getLength(), 25);
+	
+	});
+});
+
+*/
